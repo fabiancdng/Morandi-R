fe33126885f323a4ac42bfa959239d6385a667c9
fabiancdng <fabiandooblemail@gmail.com>
2021-03-20 18:46:46 +0100

    removed unnecessary files

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import config
 from quart import Quart, render_template, request, session, redirect, url_for
 from quart_discord import DiscordOAuth2Session
 from discord.ext import ipc
+from globals import modules
 
 app = Quart(__name__, static_folder="web/public", template_folder="web/templates")
 ipc_client = ipc.Client(secret_key=config.ipc_key)
@@ -43,16 +44,44 @@ async def dashboard():
     guild_ids = await ipc_client.request("get_mutual_guilds")
     user_guilds = await discord.fetch_guilds()
     user = await discord.fetch_user()
-    owned_guilds = []
+    admin_guilds = []
     mutual_guilds = []
 
     for guild in user_guilds:
-        if guild.is_owner == True and guild.id not in guild_ids:
-            owned_guilds.append(guild)
-        elif guild.is_owner == True and guild.id in guild_ids:
+        if guild.permissions.administrator == True and guild.id not in guild_ids:
+            admin_guilds.append(guild)
+        elif guild.permissions.administrator == True and guild.id in guild_ids:
             mutual_guilds.append(guild)
 
-    return await render_template("dashboard.html", owned_guilds=owned_guilds, mutual_guilds=mutual_guilds, user=user)
+    return await render_template("dashboard.html", admin_guilds=admin_guilds, mutual_guilds=mutual_guilds, user=user, config=config)
+
+@app.route("/dashboard/<guild_id>")
+async def guild_dashboard(guild_id):
+    authorized = await discord.authorized
+    if authorized != True:
+        return redirect("/")
+
+    user = await discord.fetch_user()
+    guilds = await discord.fetch_guilds()
+
+    for guild in guilds:
+        if guild.id == int(guild_id):
+            return await render_template("guild-dashboard.html", guild=guild, user=user, modules=modules)
+
+@app.route("/dashboard/<guild_id>/leveling")
+async def guild_dashboard_leveling(guild_id):
+    authorized = await discord.authorized
+    if authorized != True:
+        return redirect("/")
+    
+    user = await discord.fetch_user()
+    guilds = await discord.fetch_guilds()
+
+    for guild in guilds:
+        if guild.id == int(guild_id):
+            return await render_template("leveling.html", guild=guild, user=user)
+    
+    return redirect("/")
 
 @app.route("/logout")
 async def logout():
---
+++ globals.py
@@ -0,0 +1,22 @@
+modules = [
+    {
+        "name": "Leveling",
+        "url": "leveling",
+        "icon": "star_rate"
+    },
+    {
+        "name": "Bot Settings",
+        "url": "botsettings",
+        "icon": "smart_toy"
+    },
+    {
+        "name": "Role Reactions",
+        "url": "rolereactions",
+        "icon": "emoji_emotions"
+    },
+    {
+        "name": "Embeds",
+        "url": "embeds",
+        "icon": "chat"
+    }
+]
