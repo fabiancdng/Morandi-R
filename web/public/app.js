891a91e9d416a7f11d1c896ef97e7e609bb3794c
Fabian Reinders <fabian@fabiancdng.com>
2021-03-31 16:56:10 +0200

    optimize front-end javascript

---
+++ web/public/app.js
@@ -0,0 +1,14 @@
+$(".dropdown-trigger").dropdown({ constrainWidth: false });
+
+function save(guild_id, item, value) {
+    var formData = new FormData();
+    formData.append("item", item);
+    formData.append("value", value);
+    
+    fetch(`/api/settings/change/${guild_id}`, {method: "POST", body: formData})
+        .then(res => res.text())
+        .then(res => {
+            if(res == "done") M.toast({html: 'SUCCESS! Setting saved successfully.'});
+            else M.toast({html: 'Sorry, an error occured.'});
+    });
+}
